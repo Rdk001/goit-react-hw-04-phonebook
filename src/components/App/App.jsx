58a3d9c018505d4c0ba4accf6ea5ece141917ab2
Rdk001 <rmaslenchenko95@gmail.com>
2023-03-31 12:56:24 +0200

    completed this task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 // import { useState, useEffect } from 'react';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import ContactForm from 'components/ContactForm/ContactForm';
 import ContactsList from '../ContactsList/ContaactsList';
 import Filter from '../Filter/Filter';
@@ -67,67 +67,115 @@ import { Container } from './App,styled';
 
 // export default App;
 
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(localStorage.getItem('contacts')) ?? []
+  );
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const contacts = JSON.parse(localStorage.getItem('contacts'));
-    if (contacts) {
-      this.setState({ contacts });
-    }
-  }
-  componentDidUpdate(prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  formSubmitHandler = data => {
-    this.state.contacts.find(
-      ({ name }) => name.toLowerCase() === data.name.toLowerCase()
-    )
+  const formSubmitHandler = data => {
+    contacts.find(({ name }) => name.toLowerCase() === data.name.toLowerCase())
       ? alert(`${data.name} is already in contacts`)
-      : this.setState(prevState => ({
-          contacts: [data, ...prevState.contacts],
-        }));
+      : setContacts(prevState => [data, ...prevState]);
   };
 
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  getFiltredContacts = () => {
-    const { filter, contacts } = this.state;
+  const getFiltredContacts = () => {
     const normalizedFilter = filter.toLowerCase();
     return contacts.filter(contact =>
       contact.name.toLowerCase().includes(normalizedFilter)
     );
   };
 
-  deleteContacts = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(({ id }) => id !== contactId),
-    }));
+  const changeFilter = e => {
+    setFilter(e.currentTarget.value);
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-    return (
-      <Container>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.formSubmitHandler} contacts={contacts} />
+  const deleteContacts = contactId => {
+    setContacts(prevState => prevState.filter(({ id }) => id !== contactId));
+  };
 
-        <h2>Contacts</h2>
-        <Filter changeFilter={this.changeFilter} filter={filter} />
-        <ContactsList
-          contacts={this.getFiltredContacts()}
-          onDeleteContact={this.deleteContacts}
-        />
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={formSubmitHandler} contacts={contacts} />
+
+      <h2>Contacts</h2>
+      <Filter changeFilter={changeFilter} filter={filter} />
+      <ContactsList
+        contacts={getFiltredContacts()}
+        onDeleteContact={deleteContacts}
+      />
+    </Container>
+  );
+};
+
 export default App;
+
+// class OldApp extends Component {
+//   state = {
+//     contacts: [],
+//     filter: '',
+//   };
+
+//   componentDidMount() {
+//     const contacts = JSON.parse(localStorage.getItem('contacts'));
+//     if (contacts) {
+//       this.setState({ contacts });
+//     }
+//   }
+//   componentDidUpdate(prevState) {
+//     if (prevState.contacts !== this.state.contacts) {
+//       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+//     }
+//   }
+
+//   formSubmitHandler = data => {
+//     this.state.contacts.find(
+//       ({ name }) => name.toLowerCase() === data.name.toLowerCase()
+//     )
+//       ? alert(`${data.name} is already in contacts`)
+//       : this.setState(prevState => ({
+//           contacts: [data, ...prevState.contacts],
+//         }));
+//   };
+
+//   changeFilter = e => {
+//     this.setState({ filter: e.currentTarget.value });
+//   };
+
+//   getFiltredContacts = () => {
+//     const { filter, contacts } = this.state;
+//     const normalizedFilter = filter.toLowerCase();
+//     return contacts.filter(contact =>
+//       contact.name.toLowerCase().includes(normalizedFilter)
+//     );
+//   };
+
+//   deleteContacts = contactId => {
+//     this.setState(prevState => ({
+//       contacts: prevState.contacts.filter(({ id }) => id !== contactId),
+//     }));
+//   };
+
+//   render() {
+//     const { contacts, filter } = this.state;
+//     return (
+//       <Container>
+//         <h1>Phonebook</h1>
+//         <ContactForm onSubmit={this.formSubmitHandler} contacts={contacts} />
+
+//         <h2>Contacts</h2>
+//         <Filter changeFilter={this.changeFilter} filter={filter} />
+//         <ContactsList
+//           contacts={this.getFiltredContacts()}
+//           onDeleteContact={this.deleteContacts}
+//         />
+//       </Container>
+//     );
+//   }
+// }
+// export default App;
--- src/components/ContactsList/ContaactsList.jsx
+++ src/components/ContactsList/ContaactsList.jsx
@@ -1,28 +1,22 @@
-import { Component } from 'react';
 import { Span, DeleteItem } from './ContactList.styled';
 
-class ContactsList extends Component {
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.props.contacts.map(({ id, name, number }) => (
-            <li key={id}>
-              <Span>
-                {name}: {number}
-              </Span>
-              <DeleteItem
-                type="button"
-                onClick={() => this.props.onDeleteContact(id)}
-              >
-                Delete
-              </DeleteItem>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+const ContactsList = ({ contacts, onDeleteContact }) => {
+  return (
+    <div>
+      <ul>
+        {contacts.map(({ id, name, number }) => (
+          <li key={id}>
+            <Span>
+              {name}: {number}
+            </Span>
+            <DeleteItem type="button" onClick={() => onDeleteContact(id)}>
+              Delete
+            </DeleteItem>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default ContactsList;
